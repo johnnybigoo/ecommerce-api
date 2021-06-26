6dbb4aed214323a9a44a26261cb0dd955583b8e8
johnnybigoo <jotalico@icloud.com>
2021-06-25 22:21:01 -0400

    Model-de-Pagamento-com-Cartao-do-Juno-4

--- spec/models/juno/credit_card_payment_spec.rb
+++ spec/models/juno/credit_card_payment_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe Juno::CreditCardPayment, type: :model do
   it { is_expected.to belong_to :charge }
 
-  it { is_expected.to belong_to :order }  
-  it { is_expected.to have_many :credit_card_payments }
-
-
   it { is_expected.to validate_presence_of :key }
   it { is_expected.to validate_presence_of :release_date }
   it { is_expected.to validate_presence_of :status }
