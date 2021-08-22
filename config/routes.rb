8f155c26e599057b9607c9c48ca9f55ccbdc167c
johnnybigoo <jotalico@icloud.com>
2021-08-21 21:56:33 -0400

    Lista-e-Detalhes-dos-Pedidos-no-Admin-2

--- config/routes.rb
+++ config/routes.rb
@@ -3,35 +3,33 @@ require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
   
-  namespace :admin, defaults: { format: :json } do
+  namespace :admin do
     namespace :v1 do
       get "home" => "home#index"
       resources :categories
-      resources :coupons
+      resources :products
       resources :games, only: [], shallow: true do
         resources :licenses
       end
-      resources :products
-      resources :system_requirements
-      resources :users
+      resources :orders, only: [:index, :show]
     end
   end
-  # Class endpoint para home
+
   namespace :storefront do
     namespace :v1 do
       get "home" => "home#index"
       resources :products, only: [:index, :show]
       resources :categories, only: :index
       resources :checkouts, only: :create
-      resources :wish_items, only: [:index, :create, :destroy]
       post "/coupons/:coupon_code/validations", to: "coupon_validations#create"
       resources :games, only: :index
       resources :orders, only: [:index, :show]
+      resources :wish_items, only: [:index, :create, :destroy]
     end
   end
-
 
   namespace :juno do
     namespace :v1 do
